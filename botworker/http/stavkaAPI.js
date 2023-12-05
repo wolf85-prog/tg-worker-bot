3f0591cd5dee48945b24c1b3799d73a533a7e887
Sergey <seregaland@yandex.ru>
2023-12-05 09:57:02 +0300

    fix https

--- botworker/http/stavkaAPI.js
+++ botworker/http/stavkaAPI.js
@@ -5,6 +5,16 @@ const axios = require("axios");
 const $host_stavka = axios.create({
     baseURL: process.env.REACT_APP_API_URL_STAVKA
 })
+
+// const headers = {};
+
+// const httpsAgent = new https.Agent({
+//   ca: fs.readFileSync('./certs/cert.pem'),
+//   cert: fs.readFileSync('./certs/cert.pem'),
+// })
+
+//const data = await axios.get(url, { httpsAgent, headers })
+
 module.exports = async function getStavka(projectId, staffId) {
 //export const getStavka = async (projectId, staffId) =>{
     try {
