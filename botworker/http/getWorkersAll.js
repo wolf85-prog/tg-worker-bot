030d00e3cc7adbe5f276bc6071d8c39d05bc196d
seregey gornih <you@example.com>
2024-01-21 13:11:56 +0300

    fix

--- botworker/http/getWorkersAll.js
+++ botworker/http/getWorkersAll.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const axios = require("axios");
 
 const $host = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: process.env.REACT_APP_SERVER_URL
 })
 
 module.exports = async function getWorkersAll() {
