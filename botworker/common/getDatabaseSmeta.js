fc5ce6910007068bd68bf06ecb104446552e4f69
seregey gornih <you@example.com>
2024-03-05 11:00:05 +0300

    fix get smeta

--- botworker/common/getDatabaseSmeta.js
+++ botworker/common/getDatabaseSmeta.js
@@ -16,13 +16,13 @@ module.exports = async function getDatabaseSmeta(baseId) {
                 start: page.properties["06. Старт"].rich_text[0]?.plain_text,
                 stop: page.properties["07. Стоп"].rich_text[0]?.plain_text, 
                 chasi: page.properties["08. Часы"].rich_text[0]?.plain_text, 
-                stavka: page.properties["09. Ставка"].number,   
-                smena: page.properties["10. Смена"].number,
-                pererabotka: page.properties["11. Переработка"].number,     
-                taxi: page.properties["12. Такси"].number, 
-                gsm: page.properties["13. ГСМ"].number,  
-                transport: page.properties["14. Транспорт"].number,   
-                specialist: page.properties["16. Специалист"].number,             
+                stavka: page.properties["09. Ставка"].rich_text[0]?.plain_text,   //number
+                smena: page.properties["10. Смена"].rich_text[0]?.plain_text,   //number
+                pererabotka: page.properties["11. Переработка"].rich_text[0]?.plain_text,   //number    
+                taxi: page.properties["12. Такси"].rich_text[0]?.plain_text,   //number
+                gsm: page.properties["13. ГСМ"].rich_text[0]?.plain_text,   //number 
+                transport: page.properties["14. Транспорт"].rich_text[0]?.plain_text,   //number
+                specialist: page.properties["16. Специалист"].rich_text[0]?.plain_text,   //number            
             };
         });
 
