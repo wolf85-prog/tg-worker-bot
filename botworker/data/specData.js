2df621a99b5252f2fc215190c22614f4e375bdf0
Sergey <seregaland@yandex.ru>
2024-04-01 17:02:03 +0300

    fix

--- botworker/data/specData.js
+++ botworker/data/specData.js
@@ -6,12 +6,10 @@ module.exports = {
            icon: 'Sound',
            models: [
                {id: 1, name: 'Звукорежиссер',},
-               {id: 2, name: 'Системный инженер',},
-               {id: 3, name: 'RF-Менеджер',},
-               {id: 4, name: 'Backline',},
-               {id: 5, name: 'Roadie',},
-               {id: 6, name: 'Техник по звуку',},
-               {id: 7, name: 'RF-менеджер',},
+               {id: 2, name: 'RF-Менеджер',},
+               {id: 3, name: 'Backline',},
+               {id: 4, name: 'Roadie',},
+               {id: 5, name: 'Техник по звуку',},
            ]
        },
        {
@@ -23,7 +21,6 @@ module.exports = {
                {id: 2, name: 'Оператор световой пушки',},
                {id: 3, name: 'Гафер',},
                {id: 4, name: 'Техник по свету',},
-               {id: 5, name: 'Гаффер',},
            ]
        },
        {
@@ -35,42 +32,68 @@ module.exports = {
                {id: 2, name: 'Инженер Resolume',},
                {id: 3, name: 'Оператор Zoom',},
                {id: 4, name: 'Оператор [cameraman]',},
-               {id: 5, name: 'Техник монтажа',},
-               {id: 6, name: 'IT-специалист',},
-               {id: 7, name: 'Оператор-постановщик',},
-               {id: 8, name: 'Оператор [Cameraman]',},
+               {id: 5, name: 'Оператор-постановщик',},
+               {id: 6, name: 'Режиссер эфиров',},
+               {id: 7, name: 'Техник монтажа',},
+               {id: 8, name: 'IT-специалист',},  
            ]
        },
        {
-           id: 4,
+            id: 4,
+            name: 'PHOTO',
+            icon: 'Photo',
+            models: [
+                {id: 1, name: 'Свадебная съемка',},
+                {id: 2, name: 'Репортажная съемка',},
+                {id: 3, name: 'Портретная съемка',},
+                {id: 4, name: 'Предметная съемка',},
+                {id: 5, name: 'Документальная съемка',},
+            ]
+        },
+        {
+            id: 5,
+            name: 'PROMO',
+            icon: 'Promo',
+            models: [
+                {id: 1, name: 'Модель',},
+                {id: 2, name: 'Актер',},
+                {id: 3, name: 'Промоутер',},
+                {id: 4, name: 'Гример',},
+            ]
+        },
+        {
+            id: 6,
+            name: 'CATERING',
+            icon: 'Catering',
+            models: [
+                {id: 1, name: 'Официант',},
+                {id: 2, name: 'Бармен',},
+                {id: 3, name: 'Повар',},
+                {id: 4, name: 'Хостес',},
+                {id: 5, name: 'Банкетный менеджер',},
+            ]
+        }, 
+        {
+            id: 7,
+            name: 'STAGEHANDS',
+            icon: 'Stagehands',
+            models: [
+                {id: 1, name: 'Помощник \/ Грузчик',},
+            ]
+        },
+       {
+           id: 8,
            name: 'RIGGERS',
-           icon: 'Riggers',
+           icon: 'Stage Ground',
            models: [
                {id: 1, name: 'Верхний Риггер',},
                {id: 2, name: 'Нижний Риггер',},
+               {id: 3, name: 'Монтажник [Ground]',},
+               {id: 4, name: 'Декоратор',},
            ]
        },
        {
-           id: 5,
-           name: 'STAGEHANDS',
-           icon: 'Stagehands',
-           models: [
-               {id: 1, name: 'Помощник \/ Грузчик',},
-               {id: 2, name: 'Промоутер',},
-           ]
-       },
-       {
-           id: 6,
-           name: 'STAGE GROUND',
-           icon: 'Stage Ground',
-           models: [
-               {id: 1, name: 'Риггер [Ground]',},
-               {id: 2, name: 'Монтажник [Ground]',},
-               {id: 3, name: 'Декоратор',},
-           ]
-       },
-       {
-           id: 7,
+           id: 9,
            name: 'TRUCKS',
            icon: 'Trucks',
            models: [
@@ -81,66 +104,41 @@ module.exports = {
            ]
        },
        {
-           id: 8,
+            id: 10,
+            name: 'PARTY',
+            icon: 'Party',
+            models: [
+                {id: 1, name: 'Кавер-бенд',},
+                {id: 2, name: 'Танцевальный коллектив',},
+                {id: 3, name: 'Диджей',},
+                {id: 4, name: 'Ведущий',},
+                {id: 5, name: 'Певец \/ певица',},
+                {id: 6, name: 'Артист оригинального жанра',},
+                {id: 7, name: 'Go-Go',},
+            ]
+        },
+        {
+            id: 11,
+            name: 'GAMES',
+            icon: 'Games',
+            models: [
+                {id: 1, name: 'Квесты',},
+                {id: 2, name: 'Квизы',},
+                {id: 3, name: 'Аниматоры',},
+                {id: 4, name: 'Настольные игры \/ игровые автоматы',},
+                {id: 5, name: 'Пневмокостюмы \/ ростовые куклы',},
+                {id: 6, name: 'Активности \/ аттракционы',},
+            ]
+        },
+        {
+           id: 12,
            name: 'PRODUCTION',
            icon: 'Production',
            models: [
                {id: 1, name: 'Мероприятие под ключ',},
                {id: 2, name: 'Отдельные технические задачи',},
            ]
-       },
-       {
-           id: 9,
-           name: 'PHOTO',
-           icon: 'Photo',
-           models: [
-               {id: 1, name: 'Свадебная съемка',},
-               {id: 2, name: 'Репортажная съемка',},
-               {id: 3, name: 'Портретная съемка',},
-               {id: 4, name: 'Предметная съемка',},
-               {id: 5, name: 'Документальная съемка',},
-           ]
-       },
-       {
-           id: 10,
-           name: 'CATERING',
-           icon: 'Catering',
-           models: [
-               {id: 1, name: 'Официант',},
-               {id: 2, name: 'Бармен',},
-               {id: 3, name: 'Повар',},
-               {id: 4, name: 'Хостес',},
-               {id: 5, name: 'Банкетный менеджер',},
-           ]
-       },   
-       {
-           id: 11,
-           name: 'PARTY',
-           icon: 'Party',
-           models: [
-               {id: 1, name: 'Кавер-бенд',},
-               {id: 2, name: 'Танцевальный коллектив',},
-               {id: 3, name: 'Диджей',},
-               {id: 4, name: 'Ведущий',},
-               {id: 5, name: 'Певец \/ певица',},
-               {id: 6, name: 'Артист оригинального жанра',},
-               {id: 7, name: 'Go-Go',},
-           ]
-       },
-       {
-           id: 12,
-           name: 'GAMES',
-           icon: 'Games',
-           models: [
-               {id: 1, name: 'Аттракционы',},
-               {id: 2, name: 'Надувные фигуры',},
-               {id: 3, name: 'Игровые автоматы',},
-               {id: 4, name: 'Активности',},
-               {id: 5, name: 'Настольные игры',},
-               {id: 6, name: 'Квесты',},
-               {id: 7, name: 'Квизы',},
-           ]
-       },
+        },  
    ]
    
    }
