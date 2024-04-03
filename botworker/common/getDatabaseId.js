f0a801af41660365e05ad2fcdd5f7d2af763a9b3
seregey gornih <you@example.com>
2024-04-03 10:15:53 +0300

    fix

--- botworker/common/getDatabaseId.js
+++ botworker/common/getDatabaseId.js
@@ -9,13 +9,13 @@ module.exports = async function getDatabaseId(baseId) {
             database_id: baseId
         });
 
-        const responseResults = response.results.filter((page) => page.properties["02. Дата"].date !== null).map((page) => {
-            return {
-                id: page.id,
-                date: page.properties["02. Дата"].date?.start,
-                fio_id: page.properties["04. ФИО"].relation[0]?.id,
-                vid: page.properties["03. Вид работ"] ? page.properties["03. Вид работ"].multi_select[0]?.name : page.properties["03. Статус"].multi_select[0]?.name,
-                spec: page.properties["05. Специализация"].multi_select[0]?.name                
+        const responseResults = response.results.filter((page) => page.properties["02. Дата"] ? page.properties["02. Дата"].date : page.properties["2. Дата"].date).map((page) => {
+            return {   
+                id: page.id,        
+                date: page.properties["02. Дата"] ? page.properties["02. Дата"].date?.start : page.properties["2. Дата"].date?.start,
+                fio: page.properties["04. ФИО"] ? page.properties["04. ФИО"].relation[0]?.id : page.properties["4. ФИО"].relation[0]?.id,
+                vid: page.properties["03. Вид работ"] ? page.properties["03. Вид работ"].multi_select[0]?.name : page.properties["3. Вид работ"].multi_select[0]?.name,
+                spec: page.properties["05. Специализация"] ? page.properties["05. Специализация"].multi_select[0]?.name : page.properties["5. Специализация"].multi_select[0]?.name
             };
         });
 
