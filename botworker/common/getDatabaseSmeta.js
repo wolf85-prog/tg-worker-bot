8262834ccbbd3004a7294b001c39232bb5fc5586
Sergey <seregaland@yandex.ru>
2024-07-02 15:20:55 +0300

    fix

--- botworker/common/getDatabaseSmeta.js
+++ botworker/common/getDatabaseSmeta.js
@@ -12,17 +12,17 @@ module.exports = async function getDatabaseSmeta(baseId, smetaName) {
         const responseResults = response.results.map((page) => {
             return {
                 date: page.properties["02. Дата"]?.date?.start,
-                fio_id: page.properties["04. ФИО"]?.relation[0]?.id,
-                start: page.properties["06. Старт"]?.rich_text[0]?.plain_text,
-                stop: page.properties["07. Стоп"]?.rich_text[0]?.plain_text, 
-                chasi: page.properties["08. Часы"]?.rich_text[0]?.plain_text, 
-                stavka: page.properties["09. Ставка"]?.rich_text[0]?.plain_text,   //number
-                smena: page.properties["10. Смена"]?.rich_text[0]?.plain_text,   //number
-                pererabotka: page.properties["11. Переработка"]?.rich_text[0]?.plain_text,   //number    
-                taxi: page.properties["12. Такси"]?.rich_text[0]?.plain_text,   //number
-                gsm: page.properties["13. ГСМ"]?.rich_text[0]?.plain_text,   //number 
-                transport: page.properties["14. Транспорт"]?.rich_text[0]?.plain_text,   //number
-                specialist: page.properties["16. Специалист"]?.rich_text[0]?.plain_text,   //number            
+                // fio_id: page.properties["04. ФИО"]?.relation[0]?.id,
+                // start: page.properties["06. Старт"]?.rich_text[0]?.plain_text,
+                // stop: page.properties["07. Стоп"]?.rich_text[0]?.plain_text, 
+                // chasi: page.properties["08. Часы"]?.rich_text[0]?.plain_text, 
+                // stavka: page.properties["09. Ставка"]?.rich_text[0]?.plain_text,   //number
+                // smena: page.properties["10. Смена"]?.rich_text[0]?.plain_text,   //number
+                // pererabotka: page.properties["11. Переработка"]?.rich_text[0]?.plain_text,   //number    
+                // taxi: page.properties["12. Такси"]?.rich_text[0]?.plain_text,   //number
+                // gsm: page.properties["13. ГСМ"]?.rich_text[0]?.plain_text,   //number 
+                // transport: page.properties["14. Транспорт"]?.rich_text[0]?.plain_text,   //number
+                // specialist: page.properties["16. Специалист"]?.rich_text[0]?.plain_text,   //number            
             };
         });
 
