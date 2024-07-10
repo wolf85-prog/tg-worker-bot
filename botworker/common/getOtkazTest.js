7c83e59c91cf82d28eb5d1805660201b24a20cce
Sergey <seregaland@yandex.ru>
2024-07-10 11:59:37 +0300

    fix

--- botworker/common/getOtkazTest.js
+++ botworker/common/getOtkazTest.js
@@ -27,28 +27,31 @@ module.exports = async function getOtkazTest(bot) {
         }
     })
 
+    let j = 1
+
     if (otkazi && otkazi.length > 0) {
         console.log("Отказы ", otkazi.length)
-        otkazi.forEach(async (item, index)=> {
+        //otkazi.forEach(async (item, index)=> {
+        while (j < 5) {   
             //console.log("Цикл ", index+1)
-            const blockId = item.dataValues.blockId
-            const workerId = item.dataValues.workerId
-            const projectId = item.dataValues.projectId
-            const chatId = item.dataValues.receiverId
-            const projectName = await getProjectName(projectId)
-            const user = await Worker.findOne({where:{chatId: chatId.toString()}})
+    //         const blockId = item.dataValues.blockId
+    //         const workerId = item.dataValues.workerId
+    //         const projectId = item.dataValues.projectId
+    //         const chatId = item.dataValues.receiverId
+    //         const projectName = await getProjectName(projectId)
+    //         const user = await Worker.findOne({where:{chatId: chatId.toString()}})
 
-            // повторить с интервалом 5 секунд (проверка статуса претендента)
+    //         // повторить с интервалом 5 секунд (проверка статуса претендента)
 
-            //отправить сообщение в админ-панель
-            const text = `${user.dataValues.username}! 
-    Спасибо, что откликнулись на проект «${projectName.properties?.Name.title[0].plain_text}». В настоящий момент основной состав уже сформирован. 
-    Будем рады сотрудничеству на новых проектах!`
+    //         //отправить сообщение в админ-панель
+    //         const text = `${user.dataValues.username}! 
+    // Спасибо, что откликнулись на проект «${projectName.properties?.Name.title[0].plain_text}». В настоящий момент основной состав уже сформирован. 
+    // Будем рады сотрудничеству на новых проектах!`
 
-            const report = await bot.sendMessage('805436270', text)
-                    
+            const report = await bot.sendMessage('805436270', 'Тестовое сообщение')
+            j++       
             await delay(5000)
-        })
+        }
     }   
     
     await delay(5000)
