82484ba6fcdba51828a086bc9f45dbdafc0cad9e
Sergey <seregaland@yandex.ru>
2024-07-10 14:17:54 +0300

    fix

--- botworker/common/getOtkazTest.js
+++ botworker/common/getOtkazTest.js
@@ -33,28 +33,28 @@ module.exports = async function getOtkazTest(bot) {
         console.log("Отказы ", otkazi.length)
         //otkazi.forEach(async (item, index)=> {
         while (j < 5) {   
-            //console.log("Цикл ", index+1)
-    //         const blockId = item.dataValues.blockId
-    //         const workerId = item.dataValues.workerId
-    //         const projectId = item.dataValues.projectId
-    //         const chatId = item.dataValues.receiverId
-    //         const projectName = await getProjectName(projectId)
-    //         const user = await Worker.findOne({where:{chatId: chatId.toString()}})
+            console.log("Цикл ", j)
+            const blockId = otkazi[j].dataValues.blockId
+            const workerId = otkazi[j].dataValues.workerId
+            const projectId = otkazi[j].dataValues.projectId
+            const chatId = otkazi[j].dataValues.receiverId
+            const projectName = await getProjectName(projectId)
+            const user = await Worker.findOne({where:{chatId: chatId.toString()}})
 
-    //         // повторить с интервалом 5 секунд (проверка статуса претендента)
+            // повторить с интервалом 5 секунд (проверка статуса претендента)
 
-    //         //отправить сообщение в админ-панель
-    //         const text = `${user.dataValues.username}! 
-    // Спасибо, что откликнулись на проект «${projectName.properties?.Name.title[0].plain_text}». В настоящий момент основной состав уже сформирован. 
-    // Будем рады сотрудничеству на новых проектах!`
+            //отправить сообщение в админ-панель
+            const text = `${user.dataValues.username}! 
+    Спасибо, что откликнулись на проект «${projectName.properties?.Name.title[0].plain_text}». В настоящий момент основной состав уже сформирован. 
+    Будем рады сотрудничеству на новых проектах!`
 
-            const report = await bot.sendMessage('805436270', 'Тестовое сообщение')
+            const report = await bot.sendMessage('805436270', text)
             j++       
-            await delay(5000)
+            await delay(30000)
         }
     }   
     
-    await delay(5000)
+    //await delay(10000)
     
 }  
 
