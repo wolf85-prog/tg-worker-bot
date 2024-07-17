60bbdabed79c3a810f6c481e3d2b0015823b7411
Sergey <seregaland@yandex.ru>
2024-07-17 11:55:21 +0300

    fix filter

--- botworker/common/getOtkaz.js
+++ botworker/common/getOtkaz.js
@@ -19,7 +19,7 @@ module.exports = async function getOtkaz(bot) {
 
     const currentDate = new Date()
     //получить все запуски сканирования отказов
-    const otkazi = await Canceled.findAll({
+    const bdOtkazi = await Canceled.findAll({
         order: [
             ['id', 'ASC'],
         ],
@@ -31,84 +31,91 @@ module.exports = async function getOtkaz(bot) {
         }
     })
 
-    let j = 0
+    if (bdOtkazi && bdOtkazi.length > 0) {
+        const otkazi = bdOtkazi.filter((item)=> new Date(item.dataValues.datestart) >= currentDate || new Date(item.dataValues.dateend <= currentDate))
+        console.log("filterOtkaz: ", otkazi.length)
 
-    if (otkazi && otkazi.length > 0) {
-        console.log("Отказы ", otkazi.length)
-        //otkazi.forEach(async (item, index)=> {
-        while (j < otkazi.length) { 
-            let d = new Date()
-            d.setHours(d.getHours() + 3);
-            console.log("Цикл ", j+1, d)
-            const blockId = otkazi[j].dataValues.blockId
-            const workerId = otkazi[j].dataValues.workerId
-            const projectId = otkazi[j].dataValues.projectId
-            const chatId = otkazi[j].dataValues.receiverId
-            const projectName = await getProjectName(projectId)
-            const user = await Worker.findOne({where:{chatId: chatId.toString()}})
+        let j = 0
 
-            const cancel = await Canceled.findOne({where:{workerId, projectId, blockId}})
-            //console.log('cancel: ', cancel)
+        if (otkazi && otkazi.length > 0) {
+            console.log("Отказы ", otkazi.length)
+            //otkazi.forEach(async (item, index)=> {
+            while (j < otkazi.length) { 
+                let d = new Date()
+                d.setHours(d.getHours() + 3);
+                console.log("Цикл ", j+1, d)
+                const blockId = otkazi[j].dataValues.blockId
+                const workerId = otkazi[j].dataValues.workerId
+                const projectId = otkazi[j].dataValues.projectId
+                const chatId = otkazi[j].dataValues.receiverId
+                const projectName = await getProjectName(projectId)
+                const user = await Worker.findOne({where:{chatId: chatId.toString()}})
 
-            // повторить с интервалом 5 секунд (проверка статуса претендента)
-            if (!cancel.dataValues.cancel) {
-                //запрос в ноушен
-                const worker = await getWorkerPretendent(blockId, workerId, projectName.properties?.Name.title[0].plain_text)
-                console.log("WORKER (bot): ", worker?.status, chatId, projectName.properties?.Name.title[0].plain_text)
+                const cancel = await Canceled.findOne({where:{workerId, projectId, blockId}})
+                //console.log('cancel: ', cancel)
 
-                if (worker && worker.find(item => item.status === "Отказано")) {
-                    const currentHours = new Date(new Date().getTime()+10800000).getHours()
+                // повторить с интервалом 5 секунд (проверка статуса претендента)
+                if (!cancel.dataValues.cancel) {
+                    //запрос в ноушен
+                    const worker = await getWorkerPretendent(blockId, workerId, projectName.properties?.Name.title[0].plain_text)
+                    console.log("WORKER (bot): ", worker?.status, chatId, projectName.properties?.Name.title[0].plain_text)
 
-                    const res = await Canceled.update(
-                    { 
-                        cancel: true  
-                    },
-                    {
-                        where: {
-                            projectId: projectId,
-                            workerId: workerId,
+                    if (worker && worker.find(item => item.status === "Отказано")) {
+                        const currentHours = new Date(new Date().getTime()+10800000).getHours()
+
+                        const res = await Canceled.update(
+                        { 
+                            cancel: true  
                         },
-                    })
+                        {
+                            where: {
+                                projectId: projectId,
+                                workerId: workerId,
+                            },
+                        })
 
-                    let hello = ''
-                    if (currentHours >= 6 && currentHours < 12) {
-                        hello = 'Доброе утро'
-                    } else if (currentHours >= 12 && currentHours < 18) {
-                        hello = 'Добрый день'
-                    } else if (currentHours >= 0 && currentHours < 6) {
-                        hello = 'Доброй ночи'
-                    } else {
-                        hello = 'Добрый вечер' //18-0
+                        let hello = ''
+                        if (currentHours >= 6 && currentHours < 12) {
+                            hello = 'Доброе утро'
+                        } else if (currentHours >= 12 && currentHours < 18) {
+                            hello = 'Добрый день'
+                        } else if (currentHours >= 0 && currentHours < 6) {
+                            hello = 'Доброй ночи'
+                        } else {
+                            hello = 'Добрый вечер' //18-0
+                        }
+
+                        //отправить сообщение в админ-панель
+                        const text = `${hello}, ${user.dataValues.username}! 
+        Спасибо, что откликнулись на проект «${projectName.properties?.Name.title[0].plain_text}». В настоящий момент основной состав уже сформирован. 
+        Будем рады сотрудничеству на новых проектах!`
+
+                        const report = bot.sendMessage(chatId, text)
+                    
+                        const convId = await sendMessageAdmin(text, "text", chatId, report.message_id, null, false)
+                                                
+                        // Подключаемся к серверу socket
+                        let socket = io(socketUrl);
+                        //socket.emit("addUser", chatId)
+                        socket.emit("sendAdminSpec", {
+                            senderId: chatTelegramId,
+                            receiverId: chatId,
+                            text: text,
+                            convId: convId,
+                            messageId: report.message_id,
+                        }) 
+                        
+                        
                     }
-
-                    //отправить сообщение в админ-панель
-                    const text = `${hello}, ${user.dataValues.username}! 
-    Спасибо, что откликнулись на проект «${projectName.properties?.Name.title[0].plain_text}». В настоящий момент основной состав уже сформирован. 
-    Будем рады сотрудничеству на новых проектах!`
-
-                    const report = bot.sendMessage(chatId, text)
-                
-                    const convId = await sendMessageAdmin(text, "text", chatId, report.message_id, null, false)
-                                            
-                    // Подключаемся к серверу socket
-                    let socket = io(socketUrl);
-                    //socket.emit("addUser", chatId)
-                    socket.emit("sendAdminSpec", {
-                        senderId: chatTelegramId,
-                        receiverId: chatId,
-                        text: text,
-                        convId: convId,
-                        messageId: report.message_id,
-                    }) 
-                    
-                    
                 }
-            }
 
-           j++       
-           await delay(10000) // 10 сек
-        }
-    }           
+            j++       
+            await delay(10000) // 10 сек
+            }
+        }   
+    } else {
+        console.log("Ошибка получения из БД массива отказов")
+    }       
     
 }  
 
